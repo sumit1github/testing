6a0f29f352a69ada1153396074405ec9194deec7
sumit <sumit@micropyramid.com>
2022-08-28 11:33:51 +0530

    conf

--- conf/gunicorn_config.py
+++ conf/gunicorn_config.py
@@ -1,4 +1,9 @@
-command = '/home/sumit/Desktop/my project/testing/env/gunicorn'
-pythonpath= '/home/sumit/Desktop/my project/testing/env'
-bind='192.168.29.82:8000'
+# command = '/home/sumit/Desktop/my project/testing/env/gunicorn'
+# pythonpath= '/home/sumit/Desktop/my project/testing/env'
+# bind='192.168.29.82:8000'
+# workers=3
+
+command = '/home/sumit/env/gunicorn'
+pythonpath= '/home/sumit/env'
+bind='67.205.186.66:8000'
 workers=3
